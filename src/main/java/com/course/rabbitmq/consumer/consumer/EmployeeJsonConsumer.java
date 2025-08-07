68425440158196faa9ec50dc28b1e635e6a34791
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-07 20:15:26 +0200

    5.Consuming JSON Message (25)

---
+++ src/main/java/com/course/rabbitmq/consumer/consumer/EmployeeJsonConsumer.java
@@ -0,0 +1,26 @@
+package com.course.rabbitmq.consumer.consumer;
+
+import com.course.rabbitmq.consumer.entity.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeJsonConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeJsonConsumer.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RabbitListener(queues = "course.employee")
+    public void listen(String message) throws JsonProcessingException {
+        var emp = objectMapper.readValue(message, Employee.class);
+
+        LOG.info("Employee is {}", emp);
+    }
+}
---
+++ src/main/java/com/course/rabbitmq/consumer/entity/Employee.java
@@ -0,0 +1,60 @@
+package com.course.rabbitmq.consumer.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    @JsonProperty("employee_id")
+    private String employeeId;
+
+    private String name;
+
+    @JsonProperty("birth_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+    public Employee(String employeeId, String name, LocalDate birthDate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Employee() {
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
