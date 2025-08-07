98f1e1e636d8f229c97405e682f7322b03f76322
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-07 19:04:55 +0200

    3.Multiple Consumers For Each Queue (21)

--- src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class FixedRateConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(FixedRateConsumer.class);
 
-    @RabbitListener(queues = "course.fixedrate")
-    public void listen(String message) {
+    @RabbitListener(queues = "course.fixedrate", concurrency = "3-7")
+    public void listen(String message) throws InterruptedException {
         LOG.info("Consuming: {}", message);
+        TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(2000));
     }
 }
