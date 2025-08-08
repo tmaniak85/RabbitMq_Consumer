dafab0e1f616155768cff46c45225c4dd08f33eb
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-08 15:27:03 +0200

    10.Handle Consumer Exception with DLX (Solution One) (34)

--- src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class MyPictureImageConsumer {
         var p = objectMapper.readValue(message, Picture.class);
 
         if (p.getSize() > 9000) {
-            throw new IllegalArgumentException("Picture size too large: " + p);
+            throw new AmqpRejectAndDontRequeueException("Picture size too large: " + message);
         }
 
         LOG.info("Processing image: {}", p);
