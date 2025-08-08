0ba249f69d111661f4bb71fa278b3d3bec4c36dc
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-08 16:13:15 +0200

    11.Handle Consumer Exception with DLX (Solution Two) (35)

--- src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -1,14 +1,17 @@
 package com.course.rabbitmq.consumer.consumer;
 
 import com.course.rabbitmq.consumer.entity.Picture;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class MyPictureImageConsumer {
@@ -19,13 +22,15 @@ public class MyPictureImageConsumer {
     private ObjectMapper objectMapper;
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message) throws JsonProcessingException {
+    public void listen(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         var p = objectMapper.readValue(message, Picture.class);
 
         if (p.getSize() > 9000) {
-            throw new AmqpRejectAndDontRequeueException("Picture size too large: " + message);
+            channel.basicReject(tag, false);
         }
 
         LOG.info("Processing image: {}", p);
+
+        channel.basicAck(tag, false);
     }
 }
