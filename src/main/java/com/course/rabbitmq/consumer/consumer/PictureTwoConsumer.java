b5762ac5c06228bd3818346bb0af65d8a3eb7f89
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-08 11:40:00 +0200

    8.Topic Exchange (29)

--- src/main/java/com/course/rabbitmq/consumer/consumer/PictureTwoConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/PictureTwoConsumer.java
@@ -5,22 +5,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//@Service
-public class PictureVectorConsumer {
+@Service
+public class PictureTwoConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeJsonConsumer.class);
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @RabbitListener(queues = "q.picture.vector")
-    public void listen(String message) throws JsonProcessingException {
-        var p = objectMapper.readValue(message, Picture.class);
+    @RabbitListener(queues = {"q.picture.image", "q.picture.vector", "q.picture.filter", "q.picture.log"})
+    public void listen(Message message) throws JsonProcessingException {
+        var jsonString = new String(message.getBody());
+        var p = objectMapper.readValue(jsonString, Picture.class);
 
-        LOG.info("On picture vector: {}", p);
+        LOG.info("Consuming: {} with routing key: {}", p, message.getMessageProperties().getReceivedRoutingKey());
     }
 }
