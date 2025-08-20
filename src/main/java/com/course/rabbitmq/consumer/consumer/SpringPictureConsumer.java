cbf7c0e0777fc02cfbc336aeee3f3c3ba271721c
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-20 18:33:08 +0200

    16.Spring Retry Mechanism for Direct Exchange (48)

---
+++ src/main/java/com/course/rabbitmq/consumer/consumer/SpringPictureConsumer.java
@@ -0,0 +1,42 @@
+package com.course.rabbitmq.consumer.consumer;
+
+import com.course.rabbitmq.consumer.entity.Picture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SpringPictureConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FixedRateConsumer.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RabbitListener(queues = "q.spring.image.work")
+    public void listenImage(String message) throws IOException {
+        var picture = objectMapper.readValue(message, Picture.class);
+
+        LOG.info("Consuming image : " + picture);
+
+        if (picture.getSize() > 9000) {
+            throw new IOException("Image " + picture.getName() + " size too large : " + picture.getSize());
+        }
+
+        LOG.info("Processing image : " + picture);
+    }
+
+    @RabbitListener(queues = "q.spring.vector.work")
+    public void listenVector(String message) throws JsonProcessingException {
+        var picture = objectMapper.readValue(message, Picture.class);
+
+        LOG.info("Consuming vector : " + picture);
+        LOG.info("Processing vector : " + picture);
+    }
+}
